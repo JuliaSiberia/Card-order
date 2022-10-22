885ccfec8bf8ca54e29a7cea8a88d0ccfe80473c
Yuliya Kulpina <iyuliyaraspopina@gmail.com>
2022-10-22 18:07:34 +0800

    Commit seven

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -8,9 +9,13 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderTest {
 
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void shouldSubmitRequest() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Кульпина Юлия");
         $("[data-test-id=phone] input").setValue("+98765432101");
         $("[data-test-id=agreement]").click();
@@ -20,7 +25,6 @@ public class CardOrderTest {
 
     @Test
     void enteringInvalidNameValues() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Kulpina Yuliya");
         $("[data-test-id=phone] input").setValue("+98765432101");
         $("[data-test-id=agreement]").click();
@@ -30,11 +34,34 @@ public class CardOrderTest {
 
     @Test
     void invalidInputPhone() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Кульпина Юлия");
         $("[data-test-id=phone] input").setValue("+987654321");
         $("[data-test-id=agreement]").click();
         $("[type=button").click();
         $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void nameFieldNotFilled() {
+        $("[data-test-id=phone] input").setValue("+98765432101");
+        $("[data-test-id=agreement]").click();
+        $("[type=button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void phoneFieldNotFilled() {
+        $("[data-test-id=name] input").setValue("Кульпина Юлия");
+        $("[data-test-id=agreement]").click();
+        $("[type=button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void disagreementWithTheProcessingOfPersonalData() {
+        $("[data-test-id=name] input").setValue("Кульпина Юлия");
+        $("[data-test-id=phone] input").setValue("+98765432101");
+        $("[type=button").click();
+        $("[data-test-id=agreement].input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 }
